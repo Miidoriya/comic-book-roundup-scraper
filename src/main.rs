85e4762c90070af85d8a79d69c59df1f83239e20
Miidoriya <99621562+Miidoriya@users.noreply.github.com>
2023-01-15 15:54:12 +0000

    [initial-menu-cli] String matching using levenshtein. CLI now allows publisher and title querying to show name:url jsons structs as output

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
+use core::result::Result;
+use fuzzy_matcher::skim::SkimMatcherV2;
 use inquire::{Select, Text};
+use levenshtein::levenshtein;
 use reqwest::Error;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct Title {
+    name: String,
+    url: String,
+}
+
+impl Title {
+    fn new(name: String, url: String) -> Self {
+        Title { name, url }
+    }
+}
 
 fn do_request(url: &str) -> Result<String, Error> {
     let response = reqwest::blocking::get(url)?;
@@ -21,25 +37,42 @@ fn menu(items: &[String]) -> String {
 
 fn main() {
     loop {
-        match menu(&["Scrape publisher".into(), "Do Nothing".into(), "Exit!".into()]).as_str() {
+        match menu(&[
+            "Scrape publisher".into(),
+            "Do Nothing".into(),
+            "Exit!".into(),
+        ])
+        .as_str()
+        {
             "Scrape publisher" => {
                 let publisher = Text::new("Enter your publisher:").prompt().unwrap();
                 let document = all_series_document(&publisher).unwrap();
 
                 let title_selector = scraper::Selector::parse("td.series>a").unwrap();
 
-                let titles_vec: Vec<String> = document
-                    .select(&title_selector)
-                    .map(|x| x.inner_html())
-                    .collect();
-                let titles_count = titles_vec.len();
-                println!("The number of titles: {}", titles_count);
-                titles_vec
-                    .iter()
-                    .zip(1..titles_count + 1)
-                    .for_each(|(item, number)| println!("{}. {}", number, item));
-                
-                continue;
+                loop {
+                    let title_name = Text::new("Which title are you looking for?:")
+                        .prompt()
+                        .unwrap();
+
+                    let titles_vec: Vec<Title> = document
+                        .select(&title_selector)
+                        .map(|x| {
+                            Title::new(x.inner_html(), x.value().attr("href").unwrap().to_string())
+                        })
+                        .into_iter()
+                        .filter(|title| levenshtein(&title.name, &title_name) <= 7) // 2 is the threshold for similarity
+                        .collect();
+
+                    let titles_count = titles_vec.len();
+                    titles_vec
+                        .iter()
+                        .zip(1..titles_count + 1)
+                        .for_each(|(title, number)| {
+                            println!("{}. {}", number, serde_json::to_string(&title).unwrap())
+                        });
+                    continue;
+                }
             }
             "Do Nothing" => {
                 continue;
