7b59adf3b0956e861d96fbd2ee04a0d7733857cb
Miidoriya <99621562+Miidoriya@users.noreply.github.com>
2023-01-19 20:25:18 +0000

    [multithreading] WIP. Code not working but ongoing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
+use cli_table::{format::Justify, print_stdout, Table, WithTitle};
 use core::result::Result;
 use fuzzywuzzy::fuzz;
 use inquire::{Select, Text};
 use reqwest::Error;
 use scraper::{error::SelectorErrorKind, Html};
 use serde::{Deserialize, Serialize};
-use cli_table::{format::Justify, print_stdout, Table, WithTitle};
+use std::sync::{Arc};
 
 #[derive(Debug)]
 enum PublisherError<'a> {
@@ -42,25 +43,29 @@ struct Title {
     url: String,
 }
 
-#[derive(Debug, Serialize, Deserialize, Table)]
+#[derive(Debug, Table)]
 struct Issue {
     #[table(title = "Title", justify = "Justify::Right")]
-    title: String,
+    title: Arc<String>,
     #[table(title = "Issue Number")]
-    issue_num: String,
+    issue_num: Arc<String>,
     #[table(title = "Writer/s")]
-    writer: String,
+    writer: Arc<String>,
     #[table(title = "Artist/s")]
-    artist: String,
+    artist: Arc<String>,
     #[table(title = "User Review Score")]
-    user_review: String,
+    user_review: Arc<String>,
     #[table(title = "Critic Review Score")]
-    critic_review: String,
+    critic_review: Arc<String>,
     #[table(title = "User Review Count")]
-    user_review_count: String,
+    user_review_count: Arc<String>,
     #[table(title = "Critic Review Count")]
-    critic_review_count: String,
+    critic_review_count: Arc<String>,
 }
+
+
+unsafe impl Send for Issue {}
+
 
 impl Title {
     fn new(name: String, url: String) -> Self {
@@ -70,14 +75,14 @@ impl Title {
 
 impl Issue {
     fn new(
-        title: String,
-        issue_num: String,
-        writer: String,
-        artist: String,
-        user_review: String,
-        critic_review: String,
-        user_review_count: String,
-        critic_review_count: String,
+        title: Arc<String>,
+        issue_num: Arc<String>,
+        writer: Arc<String>,
+        artist: Arc<String>,
+        user_review: Arc<String>,
+        critic_review: Arc<String>,
+        user_review_count: Arc<String>,
+        critic_review_count: Arc<String>,
     ) -> Self {
         Issue {
             title,
@@ -120,7 +125,10 @@ fn get_publishers() -> Result<Vec<Publisher>, PublisherError<'static>> {
                     return Publisher::new("".to_string(), "".to_string()); // How can we handle this better?
                 }
             };
-            let publisher_url = format!("https://comicbookroundup.com{}/{}", publisher_elem, "all-series");
+            let publisher_url = format!(
+                "https://comicbookroundup.com{}/{}",
+                publisher_elem, "all-series"
+            );
             let publisher = Publisher::new(
                 publisher_elem.split("/").last().unwrap().to_string(),
                 publisher_url,
@@ -158,6 +166,55 @@ fn find_titles(title_name: &str, titles: &[scraper::ElementRef]) -> Vec<Title> {
         .collect()
 }
 
+fn parse_issue(named_title: &str, issue: scraper::ElementRef) -> Issue {
+    let critic_rating_selector = create_selector(".rating .CriticRatingList div");
+    let user_rating_selector = create_selector(".rating .UserRatingList div");
+    let critic_reviews_count_selector = create_selector(".reviews .CriticReviewNumList a");
+    let user_reviews_count_selector = create_selector(".reviews .UserReviewNumList a");
+    let issue_number_selector = create_selector(".issue a");
+    let writer = create_selector(".writer a");
+    let artist = create_selector(".artist a");
+    let title = named_title.to_string();
+    let issue_num = match issue.select(&issue_number_selector).next() {
+        Some(issue_num) => issue_num.inner_html(),
+        None => "N/A".to_string(),
+    };
+    let writer = match issue.select(&writer).next() {
+        Some(writer) => writer.inner_html(),
+        None => "N/A".to_string(),
+    };
+    let artist = match issue.select(&artist).next() {
+        Some(artist) => artist.inner_html(),
+        None => "N/A".to_string(),
+    };
+    let critic_review = match issue.select(&critic_rating_selector).next() {
+        Some(critic_rating) => critic_rating.inner_html(),
+        None => "N/A".to_string(),
+    };
+    let user_review = match issue.select(&user_rating_selector).next() {
+        Some(user_rating) => user_rating.inner_html(),
+        None => "N/A".to_string(),
+    };
+    let critic_review_count = match issue.select(&critic_reviews_count_selector).next() {
+        Some(critic_reviews_count) => critic_reviews_count.inner_html(),
+        None => "N/A".to_string(),
+    };
+    let user_review_count = match issue.select(&user_reviews_count_selector).next() {
+        Some(user_reviews_count) => user_reviews_count.inner_html(),
+        None => "N/A".to_string(),
+    };
+    let issue = Issue::new(
+        title.into(),
+        issue_num.into(),
+        writer.into(),
+        artist.into(),
+        user_review.into(),
+        critic_review.into(),
+        user_review_count.into(),
+        critic_review_count.into(),
+    );
+    issue
+}
 fn main() {
     loop {
         match menu(
@@ -182,16 +239,21 @@ fn main() {
                     {
                         "Exit!" => break,
                         publisher => {
-                            let current_publisher = publishers.iter().find(|publisher_obj| publisher_obj.name == publisher).unwrap();
+                            let current_publisher = publishers
+                                .iter()
+                                .find(|publisher_obj| publisher_obj.name == publisher)
+                                .unwrap();
                             println!("Scraping publisher: {}", current_publisher.url);
                             let publisher_doc = do_request(&current_publisher.url).unwrap();
-                            let publisher_parsed: Result<Html, Error> = Ok(scraper::Html::parse_document(&publisher_doc));
+                            let publisher_parsed: Result<Html, Error> =
+                                Ok(scraper::Html::parse_document(&publisher_doc));
                             let document = match publisher_parsed {
                                 Ok(document) => document,
                                 Err(e) => {
                                     println!("Error: {}", e);
                                     continue;
-                                }};
+                                }
+                            };
                             let title_name = Text::new("Which title are you looking for?:")
                                 .prompt()
                                 .unwrap();
@@ -202,6 +264,7 @@ fn main() {
                                     continue;
                                 }
                             };
+
                             let titles = find_titles(
                                 &title_name,
                                 &document
@@ -234,88 +297,43 @@ fn main() {
                                     let mut issues = document
                                         .select(&issue_selector)
                                         .collect::<Vec<scraper::ElementRef>>();
-                                    issues.drain(0..1);
-                                    let critic_rating_selector =
-                                        create_selector(".rating .CriticRatingList div");
-                                    let user_rating_selector =
-                                        create_selector(".rating .UserRatingList div");
-                                    let critic_reviews_count_selector =
-                                        create_selector(".reviews .CriticReviewNumList a");
-                                    let user_reviews_count_selector =
-                                        create_selector(".reviews .UserReviewNumList a");
-                                    let issue_number_selector = create_selector(".issue a");
-                                    let writer = create_selector(".writer a");
-                                    let artist = create_selector(".artist a");
-                                    let issues = issues.iter().map(|issue| {
-                                        let title = named_title.to_string();
-                                        let issue_num =
-                                            match issue.select(&issue_number_selector).next() {
-                                                Some(issue_num) => issue_num.inner_html(),
-                                                None => "N/A".to_string(),
-                                            };
-                                        let writer = match issue.select(&writer).next() {
-                                            Some(writer) => writer.inner_html(),
-                                            None => "N/A".to_string(),
-                                        };
-                                        let artist = match issue.select(&artist).next() {
-                                            Some(artist) => artist.inner_html(),
-                                            None => "N/A".to_string(),
-                                        };
-                                        let critic_review =
-                                            match issue.select(&critic_rating_selector).next() {
-                                                Some(critic_rating) => critic_rating.inner_html(),
-                                                None => "N/A".to_string(),
-                                            };
-                                        let user_review =
-                                            match issue.select(&user_rating_selector).next() {
-                                                Some(user_rating) => user_rating.inner_html(),
-                                                None => "N/A".to_string(),
-                                            };
-                                        let critic_review_count = match issue
-                                            .select(&critic_reviews_count_selector)
-                                            .next()
-                                        {
-                                            Some(critic_reviews_count) => {
-                                                critic_reviews_count.inner_html()
-                                            }
-                                            None => "N/A".to_string(),
-                                        };
-                                        let user_review_count =
-                                            match issue.select(&user_reviews_count_selector).next()
-                                            {
-                                                Some(user_reviews_count) => {
-                                                    user_reviews_count.inner_html()
-                                                }
-                                                None => "N/A".to_string(),
-                                            };
-                                        let issue = Issue::new(
-                                            title,
-                                            issue_num,
-                                            writer,
-                                            artist,
-                                            user_review,
-                                            critic_review,
-                                            user_review_count,
-                                            critic_review_count,
-                                        );
-                                        issue
-                                    });
-                                    let table = issues.collect::<Vec<Issue>>();
-                                    match print_stdout(table.with_title()) {
-                                        Ok(_) => println!("Success!"),
-                                        Err(e) => println!("Error: {}", e),
+                                    let mut thread_handles = vec![];
+                                    use std::sync::{Arc, Mutex};
+                                    use std::thread;
+
+                                    // create an Arc that holds a Mutex of a vec of issue_obj
+                                    let issue_objs = Arc::new(Mutex::new(vec![]));
+                                    let issue_objs_clone = issue_objs.clone();
+
+                                    for issue in issues.drain(..) {
+                                        let handle = thread::spawn(move || {
+                                            let issue_obj = parse_issue(named_title, issue);
+                                            // lock the Mutex to get a mutable reference to the vec
+                                            let mut objs = issue_objs_clone.lock().unwrap();
+                                            objs.push(issue_obj);
+                                        });
+                                        thread_handles.push(handle);
                                     }
+                                    // wait for all the thread to complete
+                                    for handle in thread_handles {
+                                        handle.join().unwrap();
+                                    }
+                                    // lock the Mutex to get a reference to the vec
+                                    let issue_objs_final = issue_objs.lock().unwrap();
                                 }
                             }
                         }
                     }
                 }
             }
-            "Exit!" => {
-                println!("Exiting CLI interface ...");
-                break;
-            }
-            _ => println!("default"),
+            "Exit!" => break,
         }
     }
 }
+
+/*
+let table = issues.collect::<Vec<Issue>>();
+                                    match print_stdout(table.with_title()) {
+                                        Ok(_) => println!("Success!"),
+                                        Err(e) => println!("Error: {}", e),
+                                    } */
